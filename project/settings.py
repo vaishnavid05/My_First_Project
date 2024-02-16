dc97454dbc218a2d7049d41859b9725c38d7f486
vaishnavid05 <143070833+vaishnavid05@users.noreply.github.com>
2024-02-16 13:09:06 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -126,7 +126,7 @@ DATABASES = {
     }
 }
 
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
